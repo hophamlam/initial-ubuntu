b3eb67f593740afd7cd7b5a98df5e7c24a066fec
hophamlam <53455661+hophamlam@users.noreply.github.com>
2023-08-16 10:06:53 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -24,10 +24,11 @@ handle_option1() {
   echo "Installing Docker..."
   curl -fsSL https://get.docker.com | bash
   echo "Installing Docker-Compose..."
-  curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
+  mkdir -p ~/.docker/cli-plugins/
+  curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+  chmod +x ~/.docker/cli-plugins/docker-compose
   echo "Check if Docker & Docker-Compose is installed"
-  docker --version && docker-compose --version
+  docker --version && docker compose version
   read -p "Installation complete. Press enter to leave"
 }
 
