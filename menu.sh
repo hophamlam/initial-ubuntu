45763329315e1b1f173e863508f4c9c13cfa2f32
hophamlam <53455661+hophamlam@users.noreply.github.com>
2023-08-16 10:02:47 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -21,16 +21,13 @@ display_menu() {
 
 # Function to handle option 1
 handle_option1() {
-  echo "Installing Docker and Docker-compose..."
-  sudo apt update && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-  sudo apt-cache policy docker-ce
-  sudo apt install docker-ce -y
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
+  echo "Installing Docker..."
+  curl -fsSL https://get.docker.com | bash
+  echo "Installing Docker-Compose..."
+  curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
   echo "Check if Docker & Docker-Compose is installed"
-  sudo docker --version && docker-compose --version
+  docker --version && docker-compose --version
   read -p "Installation complete. Press enter to leave"
 }
 
