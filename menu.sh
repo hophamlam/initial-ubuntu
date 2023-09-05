3309c348252ea067e7585df72379b84ab7cf01c1
hophamlam <53455661+hophamlam@users.noreply.github.com>
2023-09-05 10:31:58 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -49,7 +49,7 @@ handle_option2() {
 handle_option3() {
   echo "Installing Portainer CE, Portainer Agent, Wireguard VPN, Uptime Kuma"
   echo "Installing Portainer CE"
-  git -C './initial-ubuntu' pull || git clone https://github.com/hophamlam/initial-ubuntu.git './initial-ubuntu'
+  git -C '~/initial-ubuntu' pull || git clone https://github.com/hophamlam/initial-ubuntu.git '~/initial-ubuntu'
   docker volume create portainer_data
   cd ~/initial-ubuntu/docker
   nano ./docker-compose.portainer.yml && docker compose -f ./docker-compose.portainer.yml up -d
